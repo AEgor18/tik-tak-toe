4905c858ac289a41f3178ea0488b617daf7daaeb
AEgor18 <aegor246@mail.ru>
2025-01-23 17:59:42 +0300

    tenth

--- components/game-new/game.jsx
+++ components/game-new/game.jsx
@@ -2,28 +2,69 @@ import { PLAYERS } from "./constants";
 import { BackLink } from "./ui/back-link";
 import { GameInfo } from "./ui/game-info";
 import { GameLayout } from "./ui/game-layout";
+import { GameMoveInfo } from "./ui/game-move-info";
 import { GameTitle } from "./ui/game-title";
 import { PlayerInfo } from "./ui/player-info";
+import { UseGameState } from "./model/use-game-state";
+import { GameCell } from "./ui/game-cell";
+import { GameOverModal } from "./ui/game-over-modal";
+
+const PLAYERS_COUNT = 4;
 
 export function Game() {
+  const {cells, currentMove, nextMove, handleCellClick, winnerSequence, winnerSymbol} = UseGameState(PLAYERS_COUNT);
+  const winnerPlayer = PLAYERS.find(player => player.symbol === winnerSymbol)
+
   return (
-    <GameLayout
-      backLink={<BackLink />}
-      title={<GameTitle />}
-      gameInfo={
-        <GameInfo isRatingGame playersCount={4} timeMode={"1 мин на ход"} />
-      }
-      playersList={PLAYERS.map((player, index) => (
-        <PlayerInfo
-          key={player.id}
-          avatar={player.avatar}
-          name={player.name}
-          rating={player.rating}
-          seconds={60}
-          symbol={player.symbol}
-          isRight={index % 2 === 1}
-        />
-      ))}
-    />
+    <>
+      <GameLayout
+        backLink={<BackLink />}
+        title={<GameTitle />}
+        gameInfo={
+          <GameInfo isRatingGame playersCount={4} timeMode={"1 мин на ход"} />
+        }
+        playersList={PLAYERS.slice(0, PLAYERS_COUNT).map((player, index) => (
+          <PlayerInfo
+            key={player.id}
+            avatar={player.avatar}
+            name={player.name}
+            rating={player.rating}
+            seconds={60}
+            symbol={player.symbol}
+            isRight={index % 2 === 1}
+          />
+        ))}
+        gameMoveInfo={<GameMoveInfo currentMove={currentMove} nextMove={nextMove}/>}
+        gameCells={cells.map((cell, index) => (
+          <GameCell 
+            isWinner={winnerSequence?.includes(index)}
+            key={index}
+            disabled={!!winnerSymbol}
+            onClick={() => {
+              handleCellClick(index);
+            }}
+            symbol={cell}
+          />
+        ))}
+      />
+      <GameOverModal 
+        winnerName={winnerPlayer?.name}
+        players={
+          PLAYERS.slice(0, PLAYERS_COUNT).map((player, index) => (
+            <PlayerInfo
+              key={player.id}
+              avatar={player.avatar}
+              name={player.name}
+              rating={player.rating}
+              seconds={60}
+              symbol={player.symbol}
+              isRight={index % 2 === 1}
+            />
+          ))
+        }
+      />
+    </>
   );
 }
+
+
---
+++ components/game-new/ui/game-move-info.jsx
@@ -0,0 +1,15 @@
+import { GameSymbol } from "../../game/game-symbol";
+
+export function GameMoveInfo({ currentMove, nextMove }) {
+  return (
+    <>
+        <div className="flex items-center gap-1 text-xl leading-tight font-semibold">
+            Ход: <GameSymbol className="w-5 h-5" symbol={currentMove} />
+        </div>
+        <div className="flex items-center gap-1 text-xs leading-tight text-slate-400">
+            Следующий: <GameSymbol symbol={nextMove} className="w-3 h-3" />
+        </div>
+    </>
+  );
+}
+
